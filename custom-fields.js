947e73b9998b243806f62e54f7a0a0e6dccf9541
flaviogualsitedes <129856080+flaviogualsitedes@users.noreply.github.com>
2023-06-01 12:14:05 -0300

    Add files via upload

---
+++ custom-fields.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function($) {
+    // Máscara para campo de telefone
+    $(document).on('input', '[id^=_op-teln_]', function() {
+        var teln_value = $(this).val().replace(/[^0-9]/g, '');
+        var field_name = $(this).attr('id').replace('_op-teln_', '_op-tell_');
+        teln_value = teln_value.replace(/[\s\(\)\-]/g, '');
+        $('#' + field_name).val(teln_value);
+    }).keyup(function() {
+        $(this).trigger('input');
+    });
+
+    // Máscara para campo de celular
+    $(document).on('input', '[id^=_op-celn_]', function() {
+        var celn_value = $(this).val().replace(/[^0-9]/g, '');
+        var field_name = $(this).attr('id').replace('_op-celn_', '_op-cell_');
+        celn_value = celn_value.replace(/[\s\(\)\-]/g, '');
+        $('#' + field_name).val(celn_value);
+    }).keyup(function() {
+        $(this).trigger('input');
+    });
+
+    // Slug para campo de slug
+    $(document).on('input', '[id^=_op-slugn_]', function() {
+        var slugn_value = $(this).val().trim();
+        var field_name = $(this).attr('id').replace('_op-slugn_', '_op-slugl_');
+        var slugl_value = slugify(slugn_value);
+        $('#' + field_name).val(slugl_value);
+    });
+
+    function slugify(text) {
+        var slug = text.toString().toLowerCase();
+        var accents = {
+            a: /[àÀáÁâÂãäÄÅåª]/g,
+            e: /[èÈéÉêÊëË]/g,
+            i: /[ìÌíÍîÎïÏ]/g,
+            o: /[òÒóÓôÔõÕöÖº]/g,
+            u: /[ùÙúÚûÛüÜ]/g,
+            y: /[ýÝÿŸ]/g,
+			n: /[ñÑ]/g,
+			c: /[çÇ]/g
+        };
+
+        for (var letter in accents) {
+            if (accents.hasOwnProperty(letter)) {
+                slug = slug.replace(accents[letter], letter);
+            }
+        }
+
+        slug = slug.replace(/[^a-z0-9\-]/g, '-');
+        slug = slug.replace(/-+/g, '-');
+        slug = slug.replace(/^-|-$/g, '');
+
+        return slug;
+    }
+});
